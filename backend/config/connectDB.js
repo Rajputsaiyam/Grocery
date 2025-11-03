3ad92149650b1a53058239080d34c33482f96172
saiyam rajput <141070294+Rajputsaiyam@users.noreply.github.com>
2025-11-03 15:59:00 +0530

    created mongo cluster

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,11 +1,28 @@
-import mongoose, { mongo } from "mongoose";
+// import mongoose, { mongo } from "mongoose";
 
-export const connectDB = async () => {
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected ");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1); // Exit the process with failure
+//   }
+// };
+
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected ");
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit the process with failure
+    console.error("❌ MongoDB Connection Error:", error.message);
   }
 };
+
+export default connectDB;
